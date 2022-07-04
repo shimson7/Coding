7b69e059db6450549f3356b62b7c2cef3172ab49
shimson7 <101376930+shimson7@users.noreply.github.com>
2022-07-04 11:32:40 +0900

    [Silver IV] Title: 괄호, Time: 76 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.py
+++ 백준/Silver/9012. 괄호/괄호.py
@@ -1,17 +1,15 @@
-T=int(input())
-for i in range(T):
-    answer_list=list(input())
-    sum=0
-    
-    for j in answer_list:
-        if j == '(':
-            sum += 1
-        elif j == ')':
-            sum -= 1
-        if sum < 0:
-            print("NO")
-            break
-    if sum > 0:
-        print("NO")
-    elif sum == 0:
-        print("YES")
+for _ in range(int(input())):
+    s = input()
+
+    stack = []
+    for c in s:
+        if c == "(":
+            stack.append(c)
+        elif c == ")":
+            if not stack or stack[-1] != "(":
+                stack.append(")")
+                break
+
+            stack.pop()
+
+    print("YES" if not stack else "NO")
