ea19c3808a925acb74f7c50dfc0ff588b6452781
shimson7 <101376930+shimson7@users.noreply.github.com>
2022-10-14 18:19:11 +0900

    [level 1] Title: 하샤드 수, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.py
+++ 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.py
@@ -1,18 +1,10 @@
 def solution(x):
-    answer = ''
+    hashad=str(x)
     plus=0
-    num_list=[]
-    # 수를 나눠서 형변환하여 리스트로 넣어줌
-    for i in range(len(str(x))):
-        num_list.append(str(x)[i])
-    # print(num_list)
-    # 나눈 수를 형변환하여 더해줌
-    for i in range(len(num_list)):
-        plus+=int(num_list[i])
-    # print(plus)
-    if x % plus == 0:
-        answer=True
+    for i in range(len(hashad)):
+        plus+=int(hashad[i])
+    if x%plus==0:
+        answer = True
     else:
-        answer=False
-    print(answer)
+        answer = False
     return answer
