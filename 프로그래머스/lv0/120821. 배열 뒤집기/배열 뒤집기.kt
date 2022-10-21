a40115a8b8c41fd509bdd1ee6e79935d56fde135
shimson7 <101376930+shimson7@users.noreply.github.com>
2022-10-21 15:22:10 +0900

    [level 0] Title: 배열 뒤집기, Time: 14.74 ms, Memory: 65.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120821. 배열 뒤집기/배열 뒤집기.kt
@@ -0,0 +1,10 @@
+class Solution {
+fun solution(num_list: IntArray): IntArray {
+    var answer: IntArray = intArrayOf()
+    for (i in 0 until num_list.size){
+        answer = answer.plus((num_list[i]))
+    }
+    answer=answer.reversedArray()
+    return answer
+}
+}
