23069ebba719483235fd9efe8708ed8f159a1ad5
shimson7 <101376930+shimson7@users.noreply.github.com>
2023-07-03 21:23:01 +0900

    [Bronze V] Title: 코드마스터 2023, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/28235. 코드마스터 2023/코드마스터 2023.py
@@ -0,0 +1,9 @@
+answer=input()
+if answer == 'SONGDO' :
+    print('HIGHSCHOOL')
+elif answer == 'CODE' :
+    print('MASTER')
+elif answer == '2023' :
+    print('0611')
+else :
+    print('CONTEST')
