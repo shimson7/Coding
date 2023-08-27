a96a8451d8ed2082e29a68ad386c1a9a7f1d6d8c
shimson7 <101376930+shimson7@users.noreply.github.com>
2023-08-27 16:39:24 +0900

    [Bronze V] Title: Киноманы, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/28927. Киноманы/Киноманы.cc
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int t1, e1, f1, t2, e2, f2;
+  cin >> t1 >> e1 >> f1 >> t2 >> e2 >> f2;
+
+  int maxTime = 3 * t1 + 20 * e1 + 120 * f1;
+  int melTime = 3 * t2 + 20 * e2 + 120 * f2;
+
+  if (maxTime > melTime) cout << "Max\n";
+  else if (maxTime < melTime) cout << "Mel\n";
+  else cout << "Draw\n";
+
+  return 0;
+}
