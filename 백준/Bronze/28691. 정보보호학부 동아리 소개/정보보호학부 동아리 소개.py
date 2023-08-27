4df2d62351094a8340365d774ffeafc43bbc8db8
shimson7 <101376930+shimson7@users.noreply.github.com>
2023-08-27 16:36:26 +0900

    [Bronze V] Title: 정보보호학부 동아리 소개, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/28691. 정보보호학부 동아리 소개/정보보호학부 동아리 소개.py
@@ -0,0 +1,11 @@
+first_letter=input()
+if first_letter=='M':
+    print('MatKor')
+elif first_letter=='W':
+    print('WiCys')
+elif first_letter=='C':
+    print('CyKor')
+elif first_letter=='A':
+    print('AlKor')
+elif first_letter=='$':
+    print('$clear')
